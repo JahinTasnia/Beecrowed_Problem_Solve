6f9e961d186b2762d024d7a56b3b5cbc75abd17b
Jahin Tasnia <jahintasnia15@gmail.com>
2024-12-29 03:13:34 +0600

    Create Average 2.cpp

---
+++ Average 2.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    double a,b,c,x;
+    cin >> a >> b >> c;
+    x = ((a*2)+(b*3)+(c*5))/(2+3+5) ;
+    printf("MEDIA = %.1lf\n",x);
+    return 0;
+}
