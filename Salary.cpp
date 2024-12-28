9a8e1265893b1e91b7186d1929618a31d6ff6abc
Jahin Tasnia <jahintasnia15@gmail.com>
2024-12-29 03:23:50 +0600

    Create Salary.cpp

---
+++ Salary.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a,b;
+    double c;
+    cin >> a >> b >> c;
+    cout << "NUMBER = " << a << endl;
+    printf("SALARY = U$ %.2lf\n",b*c);
+    return 0;
+}
+
