2037d77e32003075ccd56358e3f9154d20275133
Jahin Tasnia <jahintasnia15@gmail.com>
2024-12-30 01:55:20 +0600

    Create Even Between five Numbers.cpp

---
+++ Even Between five Numbers.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a[5];
+    for(int i=0; i<5; i++)
+    {
+        cin >> a[i];
+    }
+
+    int even=0;
+    for(int i=0; i<5; i++)
+    {
+        if(a[i]%2==0)
+        {
+            even++;
+        }
+    }
+    cout << even << " valores pares" << endl;
+
+    return 0;
+}
