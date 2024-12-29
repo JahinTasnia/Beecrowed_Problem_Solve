79b42bc61148ff423b670a3e61e57b397efc4b66
Jahin Tasnia <jahintasnia15@gmail.com>
2024-12-29 11:53:28 +0600

    Create Sphere.cpp

---
+++ Sphere.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    double r,pi=3.14159,v;
+    cin >> r ;
+    v = (4.0/3)*pi*r*r*r ;
+    printf("VOLUME = %.3lf\n",v);
+    return 0;
+}
