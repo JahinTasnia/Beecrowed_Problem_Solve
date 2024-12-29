8500d21ba6362d5a4456696eac0f223d5d508ef4
Jahin Tasnia <jahintasnia15@gmail.com>
2024-12-29 16:56:18 +0600

    Create 	Multiples.c

---
+++ Multiples.c
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a,b;
+    cin >> a >> b ;
+    if(a>b)
+    {
+        swap(a,b);
+    }
+    if(b%a==0)
+    {
+        cout << "Sao Multiplos" << endl ;
+    }
+    else
+    {
+        cout << "Nao sao Multiplos" << endl ;
+    }
+    return 0;
+}
