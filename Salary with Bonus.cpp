245fc6f88ae038c4415cd27933b6d90f9909335c
Jahin Tasnia <jahintasnia15@gmail.com>
2024-12-29 11:40:06 +0600

    Create Salary with Bonus.cpp

---
+++ Salary with Bonus.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string st;
+    double a,b;
+    cin >> st >> a >> b ;
+    printf("TOTAL = R$ %.2lf\n", ((b*0.15)+a));
+    return 0;
+}
