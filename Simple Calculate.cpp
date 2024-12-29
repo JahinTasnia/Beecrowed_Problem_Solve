f9930429bc94bb4940de81c841b002aa38eecf2e
Jahin Tasnia <jahintasnia15@gmail.com>
2024-12-29 11:48:53 +0600

    Create Simple Calculate.cpp

---
+++ Simple Calculate.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a,b,c,d;
+    float e,f;
+    cin >> a >> b >> e;
+    cin >> c >> d >> f;
+    printf("VALOR A PAGAR: R$ %.2f\n", ((b*e)+(d*f)));
+    return 0;
+}
