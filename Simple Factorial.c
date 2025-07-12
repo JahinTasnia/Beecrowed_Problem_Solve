e440b5fc1527beafdca9d691d5b5c250f30f204b
Jahin Tasnia <jahintasnia15@gmail.com>
2025-07-12 22:36:34 +0600

    Simple Factorial.c

--- Simple Factorial.c
+++ Simple Factorial.c
@@ -1,1 +1,16 @@
 
+#include <stdio.h>
+
+int main()
+{
+
+    int n,i,factorial=1;
+    scanf("%d",&n);
+    for(i=n; i>=1; i--)
+    {
+        factorial *= i;
+    }
+
+    printf("%d\n",factorial);
+    return 0;
+}
